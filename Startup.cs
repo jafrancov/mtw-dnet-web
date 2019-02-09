1bbe765d1ac9a99f40eea0829fad14efbf6ad116
Alejandro Franco <jafrancov@MBPr-jafrancov.local>
2019-02-09 09:17:02 -0600

    Práctica 2

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace mtw_dnet_web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,11 @@ namespace mtw_dnet_web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hola MTW!");
+                await context.Response.WriteAsync("<h1>Hola MTW!</h1>");
             });
         }
     }
